7e8e6970f903c5d673b3ab7f5a2d4e6378727b7c
Matthew Gregory <matthew.e.gregory@gmail.com>
2024-03-05 02:24:27 +0000

    some styling

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get "tasks", to: "tasks#index"
+  get "tasks/new", to: "tasks#new", as: :new_tasks
+  post "tasks", to: "tasks#create", as: :create
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit
+  patch "tasks/:id", to: "tasks#update"
+  get "tasks/:id", to: "tasks#show", as: :task
+  delete "tasks/:id", to: "tasks#destroy"
 end
